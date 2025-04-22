554afe9ed2c4999793e5dd435601121a611bb359
Yogi2084 <1si22is406@sit.ac.in>
2025-04-22 14:16:48 +0530

    Update CORS middleware to allow requests from localhost:4000

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const posts: Post[] = [
 ];
 
 const allRoutes = new Hono()
-allRoutes.use(cors({ origin: 'http://localhost:3000' }))
+allRoutes.use(cors({ origin: 'http://localhost:4000' }))
 
 
 allRoutes.get('/posts', (c) => {
