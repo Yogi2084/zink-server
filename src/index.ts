30f85dfd7aa46ac71ba01f0a912cd8280cefdb5c
Yogi2084 <1si22is406@sit.ac.in>
2025-04-22 10:42:54 +0530

    Initialize project with essential configuration files and dependencies

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+serve(app)
+  console.log(`Server is running on http://localhost:${3000}`)
+
